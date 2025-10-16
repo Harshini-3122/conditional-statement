e32756e32d0ff39b5915b30f68df003affd746ef
Harshini-3122 <harshiniramesh259@gmail.com>
2025-10-16 09:46:46 +0530

    Add files via upload

---
+++ IfElseStatementTest.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IfElseStatementTest {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter any number: ");
+        int num = read.nextInt();
+        if ((num % 4) == 0) {
+            System.out.println("We are inside the true-block!");
+            System.out.println("Given number is EVEN number!!");
+        } else {
+            System.out.println("We are inside the false-block!");
+            System.out.println("Given number is ODD number!!");
+        }
+        System.out.println("We are outside the if-block!!!");
+    }
+}
